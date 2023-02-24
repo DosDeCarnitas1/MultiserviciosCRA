26e7c9668bca7cb296637e4189fba0c71711c16c
DosDeCarnitas1 <2122200365@soy.utj.edu.mx>
2023-02-24 01:04:48 -0600

    Crud de tecnicos creado

---
+++ app/src/main/java/com/example/multiservicioscra/CrudTecnicos.java
@@ -0,0 +1,164 @@
+package com.example.multiservicioscra;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CrudTecnicos extends AppCompatActivity {
+
+    EditText etnombreEmp, etapellidoEmp, etpuesto, etusuarioEmp, etcontraseñaEmp;
+    Button btnregistrar, btnconsultar, btnmodificar, btneliminar;
+
+    RequestQueue requestQueue;
+
+    String ip = "192.168.100.76";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crud_tecnicos);
+
+        etnombreEmp = findViewById(R.id.etnombreEmp);
+        etapellidoEmp = findViewById(R.id.etapellidoEmp);
+        etpuesto = findViewById(R.id.etpuesto);
+        etusuarioEmp = findViewById(R.id.etusuarioEmp);
+        etcontraseñaEmp = findViewById(R.id.etcontraseñaEmp);
+
+        btnregistrar = findViewById(R.id.btnregistrar);
+        btnconsultar = findViewById(R.id.btnconsultar);
+        btnmodificar = findViewById(R.id.btnmodificar);
+        btneliminar = findViewById(R.id.btneliminar);
+
+        btnconsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                consulta("http://" + ip + "/ejemplomovil/consultaEmp.php?codigo=" + etusuarioEmp.getText());
+            }
+        });//cierre del onclick consulta
+
+        btnregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operaciones("http://" + ip + "/ejemplomovil/insertarEmp.php");
+            }
+        });//cierre del onclick registrar
+
+        btnmodificar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operaciones("http://" + ip + "/ejemplomovil/modificaEmp.php");
+            }
+        });//cierre del onclick modificar
+
+        btneliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                operaciones("http://" + ip + "/ejemplomovil/eliminaEmp.php");
+            }
+        });//cierre del onclick eliminar
+    }//cierre del metodo oncreate
+
+    private void operaciones(String url) {
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) { //en caso de una respuesta exitosa
+                System.out.println(response);
+                if (response.equals("1")) {
+                    Toast.makeText(getApplicationContext(), "Operación exitosa", Toast.LENGTH_LONG).show();
+                    etnombreEmp.setText("");
+                    etapellidoEmp.setText("");
+                    etpuesto.setText("");
+                    etusuarioEmp.setText("");
+                    etcontraseñaEmp.setText("");
+                } else {
+                    Toast.makeText(getApplicationContext(), "Operación no exitosa", Toast.LENGTH_LONG).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) { //cuando se ejecuta la petición en el servidor y hay un error respondera con un errorRespose
+                Toast.makeText(getApplicationContext(), "Error" + error.toString(), Toast.LENGTH_LONG).show();
+                System.out.println("Error Volley: " + error.toString());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("nombre", etnombreEmp.getText().toString());
+                parametros.put("apellido", etapellidoEmp.getText().toString());
+                parametros.put("puesto", etpuesto.getText().toString());
+                parametros.put("usuario", etusuarioEmp.getText().toString());
+                parametros.put("password", etcontraseñaEmp.getText().toString());
+                parametros.put("tipo", "Empleado");
+                return parametros;
+            }
+        };
+        //remove caching
+        stringRequest.setShouldCache(false);
+        // Wait 30 seconds and don't retry more than once
+        stringRequest.setRetryPolicy(new DefaultRetryPolicy(0, 0, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        requestQueue.add(stringRequest);
+    }//cierre del método operaciones
+
+    private void consulta(String url){
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest
+                (url, new Response.Listener<JSONArray>() {
+                    //respuesta exitosa por parte del servidor
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        JSONObject jsonObject = null;
+                        for (int i = 0; i < response.length(); i++) {
+                            try {//extrayendo el objecto json de cada una de las posiciones del arreglo
+                                jsonObject = response.getJSONObject(i);
+
+                                etnombreEmp.setText(jsonObject.getString("nombre"));
+                                etapellidoEmp.setText(jsonObject.getString("apellido"));
+                                etpuesto.setText(jsonObject.getString("puesto"));
+                                etusuarioEmp.setText(jsonObject.getString("usuario"));
+                                etcontraseñaEmp.setText(jsonObject.getString("contraseña"));
+
+                            }//cierre del try
+                            catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }//cierre del for
+                    }//cierre del onResponse
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        System.out.println(error);
+                        Toast.makeText(getApplicationContext(),"Empleado no encontrado",Toast.LENGTH_LONG).show();
+
+                        etnombreEmp.setText("");
+                        etapellidoEmp.setText("");
+                        etpuesto.setText("");
+                        etusuarioEmp.setText("");
+                        etcontraseñaEmp.setText("");
+                    }
+
+                });
+        requestQueue.add(jsonArrayRequest);
+    }//cierre del método consulta
+}//cierre de la clase
--- app/src/main/java/com/example/multiservicioscra/MenuMaster.java
+++ app/src/main/java/com/example/multiservicioscra/MenuMaster.java
@@ -2,13 +2,46 @@ package com.example.multiservicioscra;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MenuMaster extends AppCompatActivity {
+
+    Button btntickets, btntecnicos, btnrefacciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_master);
+
+        btntickets = findViewById(R.id.btntickets);
+        btntecnicos = findViewById(R.id.btntecnicos);
+        btnrefacciones = findViewById(R.id.btnrefacciones);
+
+        btntecnicos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent v = new Intent(getApplicationContext(), CrudTecnicos.class);
+                startActivity(v);
+            }
+        });
+
+        //btntickets.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+           // public void onClick(View view) {
+              //  Intent v = new Intent(getApplicationContext(), CrudTickets.class);
+              //  startActivity(v);
+          //  }
+       // });
+
+       // btnrefacciones.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+         //   public void onClick(View view) {
+         //       Intent v = new Intent(getApplicationContext(), CrudRefacciones.class);
+         //       startActivity(v);
+          //  }
+       // });
     }
 }
